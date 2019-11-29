c4c7d4e08d844486fb6017e917fdf4bea896806c
Boris Arkenaar <boris@arkenaar.com>
2019-11-29 16:43:11 +0100

    Create empty entries component

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Entries from '../src/entries';
+
+export default () => (
+  <div>
+    <h1>Hello World!</h1>
+    <Entries />
+  </div>
+);
---
+++ src/entries/Entries.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <h1>Entries list</h1>;
