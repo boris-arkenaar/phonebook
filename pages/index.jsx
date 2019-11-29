fe70a6696b55866c362d683ab2990b2e6ad7f7a2
Boris Arkenaar <boris@arkenaar.com>
2019-11-29 18:29:53 +0100

    Add material-ui and material-table

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import Head from 'next/head';
 import Entries from '../src/entries';
+import 'whatwg-fetch';
 
 export default () => (
   <div>
+    <Head>
+      <title>Phonebook</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      />
+    </Head>
     <h1>Hello World!</h1>
     <Entries />
   </div>
--- src/entries/Entries.jsx
+++ src/entries/Entries.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import MaterialTable from "material-table";
+import { TextField } from "@material-ui/core";
 
 export default () => {
   const [entries, setEntries] = useState([]);
@@ -14,5 +15,44 @@ export default () => {
     fetchEntries();
   }, []);
 
-  return <h1>Entries list ({entries.length})</h1>;
+  return (
+    <div>
+      <h1>Entries list ({entries.length})</h1>
+      <div style={{ maxWidth: "100%" }}>
+        <MaterialTable
+          columns={[
+            {
+              title: "First name",
+              field: "firstName",
+              editComponent: props => (
+                <TextField
+                  error={!props.value}
+                  value={props.value}
+                  onChange={e => props.onChange(e.target.value)}
+                />
+              )
+            },
+            { title: "Last name", field: "lastName" },
+            { title: "Phone number", field: "phoneNumber" }
+          ]}
+          data={entries}
+          title={`Phonebook entries (${entries.length})`}
+          editable={{
+            onRowAdd: newData =>
+              new Promise(resolve => {
+                resolve();
+              }),
+            onRowUpdate: (newData, oldData) =>
+              new Promise((resolve, reject) => {
+                reject();
+              }),
+            onRowDelete: oldData =>
+              new Promise(resolve => {
+                resolve();
+              })
+          }}
+        />
+      </div>
+    </div>
+  );
 };
